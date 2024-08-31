c1fef8b8dee5c27711fc4dfc34163ae098c1b7ce
safrot1 <mostafam890u@gmail.com>
2024-08-31 05:24:31 +0300

    Update and rename convertidor-tomp3.js to لصوت.js

---
+++ plugins/لصوت.js
@@ -0,0 +1,27 @@
+import { toAudio } from '../lib/converter.js' 
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+    let q = m.quoted ? m.quoted : m
+    let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
+    
+    if (!/video|audio/.test(mime)) throw '*\`『 اعمل ريب ع الفديو الي هحولو لي صوت🧚🏻‍♂️ 』\`*'
+
+    await conn.sendPresenceUpdate('recording', m.chat)
+    let media = await q.download?.()
+
+    if (!media && !/video/.test(mime)) throw `مش لاقي الملف، تأكد إنه فيديو وحاول تاني.`
+    if (!media && !/audio/.test(mime)) throw `مش لاقي الملف، تأكد إنه صوت وحاول تاني.`
+
+    let audio = await toAudio(media, 'mp4')
+
+    if (!audio.data && !/audio/.test(mime)) throw `مش قادر أحول الملف لصوت، تأكد إنه ملف صوتي.`
+    if (!audio.data && !/video/.test(mime)) throw `مش قادر أحول الملف لصوت، تأكد إنه ملف فيديو.`
+
+    conn.sendFile(m.chat, audio.data, 'error.mp3', '', m, null, { mimetype: 'audio/mp4' })
+}
+
+handler.help = ['تحويل (رد)']
+handler.tags = ['الصوت']
+handler.command = ['تحويل', 'تحويل_الصوت', 'mp3']
+
+export default handler
