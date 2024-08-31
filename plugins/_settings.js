a898d532ed33a40f40486a20ce1e1ae0277aff14
safrot1 <mostafam890u@gmail.com>
2024-08-31 07:12:15 +0300

    Update _settings.js

--- plugins/_settings.js
+++ plugins/_settings.js
@@ -9,20 +9,20 @@ handler.all = async function (m) {
 
 //------------------------------------------------
 
-global.FakeChannel = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363192435027853@newsletter', newsletterName: "『✯ Team Comunity Natsuki 』", serverMessageId: -1 }
+global.قناةوهمية = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363192435027853@newsletter', newsletterName: "『✯ بوت سفروت الفقدان 』", serverMessageId: -1 }
 }}, { quoted: m }
 
 //-----------------------------------------------
 
-global.Fakechannel = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363192435027853@newsletter', newsletterName: "『✯ Team Comunity Natsuki 』", serverMessageId: -1 }, forwardingScore: 999, externalAdReply: { title: gt, body: wm, thumbnailUrl: fantasyImg.getRandom(), sourceUrl: md, mediaType: 1, renderLargerThumbnail: false
+global.قناةوهميةبالتفاصيل = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363192435027853@newsletter', newsletterName: "『✯ بوت سفروت الفقدان 』", serverMessageId: -1 }, forwardingScore: 999, externalAdReply: { title: gt, body: wm, thumbnailUrl: fantasyImg.getRandom(), sourceUrl: md, mediaType: 1, renderLargerThumbnail: false
 }}}, { quoted: m }
 
 //--------------------------------------------------
-var canal = [
-{ link: canalofc, id: "120363192435027853@newsletter", name: "『✯ Team Comunity Natsuki 』" }]
-var channelsLPD = canal
+var قنوات = [
+{ link: قناةرسمية, id: "120363192435027853@newsletter", name: "『✯ بوت سفروت الفقدان 』" }]
+var قنواتوا = قنوات
 
-global.channelWa = { contextInfo: { mentionedJid: null, forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelsLPD.id, serverMessageId: '', newsletterName: channelsLPD.name }, externalAdReply: { title: wm, body: gt, mediaType: 1, renderLargerThumbnail: false, previewType: `PHOTO`, thumbnailUrl: fantasyImg.getRandom(), thumbnail: imagen1, sourceUrl: canalofc }}}, { quoted: m }
+global.قناةواتساب = { contextInfo: { mentionedJid: null, forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: قنواتوا.id, serverMessageId: '', newsletterName: قنواتوا.name }, externalAdReply: { title: wm, body: gt, mediaType: 1, renderLargerThumbnail: false, previewType: `PHOTO`, thumbnailUrl: fantasyImg.getRandom(), thumbnail: imagen1, sourceUrl: قناةرسمية }}}, { quoted: m }
 
 }
 export default handler
