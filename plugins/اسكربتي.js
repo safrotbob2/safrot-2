e49f32413ac5ba3e1c1427af40f9850feb10dfee
safrot1 <mostafam890u@gmail.com>
2024-08-31 16:40:38 +0300

    Update and rename propietario(a)-update.js to اسكربتي.js

---
+++ plugins/اسكربتي.js
@@ -0,0 +1,22 @@
+import { execSync } from 'child_process';
+
+let handler = async (m, { conn, text }) => {
+  try {
+    if (global.conn.user.jid == conn.user.jid) {
+      let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
+      conn.reply(m.chat, stdout.toString(), m);
+    }
+    // } catch {
+    var update = execSync('git remote set-url origin https://github.com/Wilsmac/NatsukiBot.git && git pull');
+    await m.reply(update.toString());
+  } catch {
+    await m.reply(`في مشكلة في التحديث`);
+  }
+};
+
+handler.help = ['اسكربتي'];
+handler.tags = ['مالك'];
+handler.command = /^اسكربتي|actualizar$/i;
+
+
+export default handler;
